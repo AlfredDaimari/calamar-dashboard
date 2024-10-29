dbff146672924630e6b773241e2ae9c64c384cdd
AlfredDaimari <alfred_daimari@yahoo.com>
2024-10-29 22:19:01 +0100

    mv: moved tests

--- src/calamar_backend/tests.sh
+++ src/calamar_backend/tests.sh
@@ -4,11 +4,11 @@
 #   run using -> pipenv run bash tests.sh
 
 export PYTHONPATH=.
-export CALAMAR_DB=/home/alfred/Code/projects/calamar_dashboard/src/calamar_backend/.temp/database.db
-export TICKER_MAP=/home/alfred/Code/projects/calamar_dashboard/src/calamar_backend/.temp/map.yaml
+export CALAMAR_DB=/home/alfred/Code/projects/calamar_dashboard/src/.temp/database.db
+export TICKER_MAP=/home/alfred/Code/projects/calamar_dashboard/src/.temp/map.yaml
 export ZERODHA_TRADE_REPORT=/home/alfred/Code/projects/calamar_dashboard/data/portfolio.csv
 export ZERODHA_BANK_STATEMENT=/home/alfred/Code/projects/calamar_dashboard/data/ledger-FP9847.csv
-export CALAMAR_CSV_DB=/home/alfred/Code/projects/calamar_dashboard/src/calamar_backend/.temp
+export CALAMAR_CSV_DB=/home/alfred/Code/projects/calamar_dashboard/src/.temp
 
 # add tests to run
 tests=(tests/database.py tests/utils.py tests/database_csv.py)
