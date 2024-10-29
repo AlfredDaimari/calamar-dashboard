e73333d894a7b97f17f61ee407f15777795f8c6d
AlfredDaimari <alfred_daimari@yahoo.com>
2024-10-29 17:14:13 +0100

    feat: better testing for database csv

--- src/calamar_backend/calamar_backend/database_csv.py
+++ src/calamar_backend/calamar_backend/database_csv.py
@@ -7,6 +7,7 @@ import pathlib
 from calamar_backend.price import download_price as yf_download_price
 from calamar_backend.maps import TickerMap
 from calamar_backend.interface import Time
+from calamar_backend.errors import DayClosePriceNotFoundError
 
 
 class DatabaseCSV:
@@ -25,13 +26,16 @@ class DatabaseCSV:
         self.mem_slots = mem_slots
         self.lru: list[tuple[str, int, pd.DataFrame]] = []
 
+    def get_csv_file_path(self, ticker_fy: tuple[int, str]) -> str:
+        return f"{self.csv_dir_path}/{ticker_fy[-1]}_{ticker_fy[0]}"
+
     def __file_exists(self, ticker_fy: tuple[str, int]) -> bool:
         csv_file = pathlib.Path(
             f"{self.csv_dir_path}/{ticker_fy[0] + '_' + str(ticker_fy[-1])}"
         )
         return csv_file.is_file()
 
-    def __lru_find_file(self, ticker_fy: tuple[str, int]) -> int:
+    def __lru_find_dataframe(self, ticker_fy: tuple[str, int]) -> int:
         """
         Get df location if it exists in memory
 
@@ -56,19 +60,22 @@ class DatabaseCSV:
         :parameter ticker_fy: (ticker, int)
         """
         [ticker, fy] = ticker_fy
-        mem_loc = self.__lru_find_file(ticker_fy)
+        mem_loc = self.__lru_find_dataframe(ticker_fy)
 
         if mem_loc == -1:
             if len(self.lru) == self.mem_slots:
                 self.lru.pop(0)
                 self.lru.append((ticker, fy, df))
+
             else:
                 self.lru.append((ticker, fy, df))
         else:
             self.lru.pop(mem_loc)
             self.lru.append((ticker, fy, df))
 
-    def read(self, ticker: str, date: datetime.datetime) -> pd.Series | None:
+    def read(
+        self, ticker: str, date: datetime.datetime
+    ) -> tuple[int, pd.Series | None]:
         """
         - get fy year
         - check if file exists
@@ -77,26 +84,37 @@ class DatabaseCSV:
         - return series or None
 
         Returns:
-            df: pd.Dataframe
-            None: when data does not exist
+        [int, pd.Series| None]
+        the integer variable is the location of df in LRU
         """
+
+        loc: int = -1  # location of DF in LRU
         fy = Time.date_fy(date)
         ret: pd.Series
 
         if self.__file_exists((ticker, fy)):
-            loc = self.__lru_find_file((ticker, fy))
+            loc = self.__lru_find_dataframe((ticker, fy))
 
-            if loc != -1:
-                df = self.lru[loc][-1]
-                ret = df.loc[Time.convert_date_to_strf(date)]
-                self.lru_append_data((ticker, fy), df)
+            try:
+                if loc != -1:
+                    df = self.lru[loc][-1]
+                    self.lru_append_data((ticker, fy), df)
+                    ret = df.loc[Time.convert_date_to_strf(date)]
 
-            else:
-                df = pd.read_csv(f"{self.csv_dir_path}/{ticker}_{fy}")
-                df["Date"] = pd.to_datetime(df["Date"])
-                df = df.set_index("Date")
-                self.lru_append_data((ticker, fy), df)
-                ret = df.loc[Time.convert_date_to_strf(date)]
+                else:
+                    df = pd.read_csv(f"{self.csv_dir_path}/{ticker}_{fy}")
+                    df["Date"] = pd.to_datetime(df["Date"])
+                    df = df.set_index("Date")
+                    self.lru_append_data((ticker, fy), df)
+                    ret = df.loc[Time.convert_date_to_strf(date)]
+
+            except KeyError:
+                raise DayClosePriceNotFoundError
+
+            except Exception:
+                raise Exception(
+                    f"{datetime.datetime.now(): db_csv.read({ticker},{Time.convert_date_to_strf(date)}): something went wrong}!"
+                )
 
         else:
             [start, end] = Time.date_fy_start_end(date)
@@ -109,6 +127,16 @@ class DatabaseCSV:
             )
 
             self.lru_append_data((ticker, fy), df)
-            ret = df.loc[Time.convert_date_to_strf(date)]
 
-        return ret
+            try:
+                ret = df.loc[Time.convert_date_to_strf(date)]
+
+            except KeyError:
+                raise DayClosePriceNotFoundError
+
+            except Exception:
+                raise Exception(
+                    f"{datetime.datetime.now()}: db_csv.read({ticker},{Time.convert_date_to_strf(date)}): something went wrong!"
+                )
+
+        return (loc, ret)
--- src/calamar_backend/tests/database_csv.py
+++ src/calamar_backend/tests/database_csv.py
@@ -1,13 +1,41 @@
 import timeit
+import os
 from calamar_backend import interface as inf
 from calamar_backend import database_csv as db
 
 
 def test_read() -> bool:
     try:
-        db_ = db.DatabaseCSV(5)
-        d = inf.Time("2023-10-05 00:00:00")
-        output = db_.read("reliance", d.date)
+        ticker = "reliance"
+        db_ = db.DatabaseCSV(3)
+        dates = {
+            "d24": inf.Time("2023-10-05 00:00:00"),
+            "d23": inf.Time("2022-10-06 00:00:00"),
+            "d22": inf.Time("2021-10-04 00:00:00"),
+            "d21": inf.Time("2020-10-07 00:00:00"),
+        }
+
+        # removing files for better tests
+        for key in dates:
+            fy: int = inf.Time.date_fy(dates[key].date)
+            file_path = db_.get_csv_file_path((fy, ticker))
+
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+        # reading files
+        output = []
+        dates_keys = list(dates.keys())
+        for i in range(len(dates_keys)):
+            [loc, serie] = db_.read(ticker, dates[dates_keys[i]].date)
+            output.append(serie)
+            assert loc == -1  # all files show be downloaded from yf
+
+        [loc, _] = db_.read(ticker, dates["d21"].date)
+        assert loc == 2
+        [loc, _] = db_.read(ticker, dates["d24"].date)
+        assert loc == -1
+
         print(f"\ntest_read_results:{output}")
 
     except Exception as e:
